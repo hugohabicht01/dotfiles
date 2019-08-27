d85c3d87241d62a22439462718443b7c068453a9
linuxer <hugohabicht01@gmx.net>
2019-08-27 22:14:48 +0200

    Added script for  getting all configs

--- getConfig.fish
+++ getConfig.fish
@@ -1,11 +1,19 @@
-set configs ~/.config
-set dotfiles ~/dotfiles/.config
+#!/usr/bin/fish
+#
+#TODO: change this if and else to set destination and change format of file
+#
+#
+set configs ~
+set dotfiles ~/dotfiles
 
-set dirs (ls $dotfiles)
-for dir in $dirs
-	echo Copying $dir
-	cp -rf $configs/$dir $dotfiles
-	echo $configs/$dir
-	echo $dotfiles/$dir
-end
+# for filename in $files
+# 	echo Copying $files \n
+# 	# echo $configs/$files \n
+# 	# echo $dotfiles/$files \n
+# end
 
+while read filename
+	cp -f $configs/$filename $dotfiles/$filename
+	echo $configs/$filename
+	echo $dotfiles/$filename
+end < configFiles
